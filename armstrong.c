dc1053652b776706b9d5e5273c0b96d175399561
Renato Cordeiro Ferreira <renatocordeiroferreira@gmail.com>
2013-04-02 14:54:18 -0300

    Initial commit

---
+++ armstrong.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include<getopt.h>
+
+int main(int argc, char **argv)
+{
+    
+    if(argv != 2) 
+        printf("Erro: número incorreto de argumentos\n");
+    
+    
+    return 0;
+}
