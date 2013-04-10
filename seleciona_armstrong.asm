5bcd29327665bf794520a625ccd715a740d2ef8c
Renato Cordeiro Ferreira <renatocordeiroferreira@gmail.com>
2013-04-10 13:32:17 -0300

    I/O working

--- seleciona_armstrong.asm
+++ seleciona_armstrong.asm
@@ -47,28 +47,32 @@ extern fprintf
 
 ;_exponencial:
 
-   
-
 
 section .text
 seleciona_armstrong:
 
-    pusha               ; empilha valores dos registradores
     push    ebp         ; empilha endereco da base da pilha
     mov     ebp, esp    ; muda base da pilha
-    ; sub     esp, 8
-    ; mov eax,
+    
+    
+    mov     eax,[ebp+8]   ; Salva endereco da entrada
+    mov     [i_addr],eax
+    
+    mov     eax,[ebp+12]  ; Salva endereco da saida
+    mov     [o_addr],eax
+    
+    pusha               ; empilha valores dos registradores
 
     ; Idêntico ao fopen(file_name, file_mode)
-    push    read_mode
-    push    file_name
+    push    DWORD read_mode
+    push    DWORD [i_addr] ;file_name
     call    fopen
     mov     [fpi], eax ;
     add     esp,8
 
     ; Idêntico ao fopen(file_name, file_mode)
-    push    write_mode
-    push    out_name
+    push    DWORD write_mode
+    push    DWORD [o_addr] ;out_name
     call    fopen
     mov     [fpo], eax ;
     add     esp,8
@@ -92,8 +96,7 @@ scanf:
     add     esp,12
     ;call    _calcula	; chamada de para verficar se é um numero de armstrong
     
-    mov     DWORD [lido],eax
-    cmp     DWORD [lido], -1
+    cmp     eax,-1
     je      fim 
     
 printf:
@@ -104,7 +107,6 @@ printf:
     add     esp,12
     
     jne     scanf
-    ; jmp fim
 
 fim:    
     ; int fclose(FILE *string)
@@ -118,22 +120,16 @@ fim:
     add     esp,4
     
     ; restaura as condicoes iniciais
-    pop     eax      ; valor original de ebp em eax
-    xchg    ebp,eax  ; troca ebp com eax
     popa             ; desempilha antigos valores dos registradores
+    pop     ebp      ; valor original de ebp em eax
     
-    ; void exit(int status);
-    mov     eax,1   ; numero da chamada ao sistema (exit)
-    mov     ebx,0   ; primeiro argumento: codigo de saida (sucesso)
-    int     80h     ; chamada ao nucleo do SO
+    ret 
 
 section .data                     ; Declaração de variaveis.
 
-file_name:  db "entrada", 0  ; alterar pra pegar do código C depois
-out_name:   db "saida", 0
+inteiro:    db "%d ",0
 read_mode:  db "r", 0
 write_mode: db "w", 0
-inteiro:    db "%d ",0
 ;contadigito   db 1,0
 
 o_pos:      db   12
@@ -141,37 +137,14 @@ i_pos:      db   16
 
 ; endereço das varáveis a ser calculada
 
-;ndigitos: equ +12
-;Digit1: equ  +16
-;Digit2: equ  +20
-;Digit3: equ  +24
-;Digit4: equ  +28
-;Digit5: equ  +32
-;Digit6: equ  +36
-;Digit7: equ  +40
-;Digit8: equ  +44
-;Digit9: equ  +48
-
-;variáveis locais
-
-;temp1: equ -4
-;temp2: equ -8
-;temp3: equ -12
-;temp4: equ -16
-;temp5: equ -20
-;Temp6: equ -24
-;temp7: equ -28
-;temp8: equ -32
-;temp9: equ -36
-;temp10: equ -40
-
 section .bss
 
 ;quociente:	resd	1
+fpi:     resd   1
+fpo:     resd   1
 soma:	 resd   1
 numero:  resd   1
-fpi:     resd   1
 i_name:  resd   1
-fpo:     resd   1
 o_name:  resd   1
-lido:    resd   1
+i_addr:  resd   1
+o_addr:  resd   1
